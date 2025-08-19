f22a946092038865ac1ac51ab74fdebfc3d0fdd9
Loris <loris.oppia@devinterface.com>
2025-08-19 17:31:16 +0200

    Add page chi-siamo

--- pitagoraDev/urls.py
+++ pitagoraDev/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pitagoraDev.views import *
 
 urlpatterns = [
-  path('', Home.as_view())
+  path('', Home.as_view()),
+  path('chi-siamo', ChiSiamo.as_view())
 ] 
--- pitagoraDev/views.py
+++ pitagoraDev/views.py
@@ -4,19 +4,19 @@ from rest_framework.views import APIView
 class Home(APIView):
 
   def get(self, request):
-    # alessandro@logopedistarigamonti.com, tag a con mail to      OK
-    # psicologia dell'età evolutiva / Tutor DSA       OK
-    # i nostri servizi, metti foto figure con sfondo bianco, e in mezzo il blu, arancione ecc
-    # invece del hr metti 4 pallini con i 4 colori, 87B5C4, C44325, F8B451, 000000      OK
-    # nel benvenuti al...metto il portfolio di ale
-    # sistemare foto ale nella seconda pagina
-
-    # copia spudoratamente la pagina contatti
-    # nel chi siamo link a "perchè sceglierci"
-    # mettilo online su mazino
-    # fai footer
-
-    # nei servizi copia da alke e metti foto e testi a caso
-    # navbar tutto maiuscolo    OK
-
     return render(request, 'home.html')
+
+class ChiSiamo(APIView):
+
+  def get(self, request):
+    return render(request, 'chi_siamo.html')
+  
+
+# COSE DA FARE
+# 1 - nei nostri servizi metti foto figure (triangolo, quadrato, pentagono ecc...) con sfondo bianco e in mezzo il blu, arancione ecc...
+# 2 - copia la pagina contatti
+# 4 - fai footer OK
+# 5 - nel benvenuti al metto il portfolio di ale
+# 6 - sistemare foto ale nella seconda pagina
+# 7 - metti online su mazino
+# 8 - fai pagina chi siamo da gmail
